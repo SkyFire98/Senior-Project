dc7c9cfcd39a8bf09dbc829efd0ff58879df8392
camathews <chanmathews@gmail.com>
2020-02-21 13:35:28 -0600

    Add files via upload

---
+++ db_test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+import MySQLdb
+
+db = MySQLdb.connect(host="localhost",  # your host 
+                     user="root",       # username
+                     passwd="insert pass",     # password
+                     db="python_test")   # name of the database
+
+# Create a Cursor object to execute queries.
+cur = db.cursor()
+
+# Select data from table using SQL query.
+cur.execute("SELECT * FROM examples")
+
+# print the first and second columns      
+for row in cur.fetchall() :
+    print row[0], " ", row[1]
