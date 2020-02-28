4efd0fcb862e34aec3f6c8dc0ef8242adcf712a7
PEWEBER <peweber@mail.lipscomb.edu>
2020-02-28 12:47:14 -0600

    Updated db_insert.py
    Insert into dynamic db

--- db_insert.py
+++ db_insert.py
@@ -7,7 +7,7 @@ api = shodan.Shodan(SHODAN_API_KEY)     # Create API object.
 results = api.search(query, limit=150)     # Make API call.
 
 # Open database connection
-db = pymysql.connect("localhost", "user", "password", "database", autocommit=True)
+db = pymysql.connect("localhost", "user", "password", "db", autocommit=True)
 # prepare a cursor object using cursor() method
 cursor = db.cursor()
 
@@ -15,73 +15,62 @@ cursor = db.cursor()
 
 for result in results['matches']:
     try:
-        # print("ip_str: " + str(result['ip_str']))
         ip_str = result['ip_str']
     except:
-        # print("ip_str: NULL")
         ip_str = None 
 
     try:
-        # print("port: " + str(result['port']))
         port = result['port']
     except:
-        # print("port: NULL")
         port = None
 
     try:
-        # print("transport: " + str(result['transport']))
         transport = result['transport']
     except:
-        # print("transport: NULL")
         transport = None
 
     try:
-        # print("product: " + str(result['product']))
         product = result['product']
     except:
-        # print("product: NULL")
         product = None
 
     try:
-        # print("version: " + str(result['version']))
         version = result['version']
     except:
-        # print("version: NULL")
         version = None
 
     try:
-        # print("server: " + str(result['http']['server']))
         server = result['http']['server']
     except:
-        # print("server: NULL")
         server = None
 
+    
+
+    #FILL asset_info TABLE
+    cursor.execute("Insert into asset_info(ip_str, port, transport, product, version, server) values('%s', %d, '%s', '%s', '%s', '%s')"% (ip_str, int(port), transport, product, version, server))
+    db.commit()	
+    entry_id=cursor.lastrowid
+
+
+    #FILL cve_info TABLE and cve_references
     try:
-        vuln_count = 0
+        
         for cve in result['vulns']:
-            if (vuln_count == 3):
-                break
-            # print("\n" + cve)
-            # print("\t" + str(result['vulns'][cve]['summary']))
-            vuln_count = vuln_count + 1
-        vuls = vuln_count
+
+            sum = result['vulns'][cve]['summary']
+
+            cursor.execute("Insert into cve_info(entry_id, cve, summary) values(%d, '%s', '%s')"% (entry_id, cve, sum))
+            db.commit()	
+            cve_id=cursor.lastrowid
+
+            for reference in result['vulns'][cve]['references']:
+                cursor.execute("Insert into cve_references(cve_id, reference) values(%d, '%s')"% (cve_id, reference))
+                db.commit()	
+        
+
+                
     except:
-        # print("vulns: NULL")
-        vulns = None
+        continue
 
-    '''print(port)
-    print(transport)
-    print(product)
-    print(version)
-    print(server)
-    print(vulns)'''
-    # execute SQL query using execute() method.
-    cursor.execute("Insert into vul values('%s', '%s', '%s', '%s', '%s', '%s', '%s')"% (ip_str, port, transport, product, version, server, vulns))
-    db.commit()
-
-    # Fetch a single row using fetchone() method.
-    data = cursor.fetchone()
-    print(data)
-
-    # disconnect from server
+# disconnect from server
 db.close()
