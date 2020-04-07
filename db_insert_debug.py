718d8d9069ab73cdf35899f57a2f2dd0d8b6eff0
PEWEBER <peweber@mail.lipscomb.edu>
2020-04-07 14:35:24 -0500

    Updated db_insert.py
    Took out reference table inserts. Table no longer needed.

--- db_insert_debug.py
+++ db_insert_debug.py
@@ -1,13 +1,13 @@
 import shodan
 import pymysql
 
-SHODAN_API_KEY = "Insert API Key"     # Shodan.io API key for auth.
+SHODAN_API_KEY = "UUFuyEaMCkYneMqmlNTImLNl5CwLdpGl"     # Shodan.io API key for auth.
 query = "net:129.59.0.0/16"    # The service to query for.
 api = shodan.Shodan(SHODAN_API_KEY)     # Create API object.
-results = api.search(query, limit=199)     # Make API call.
+results = api.search(query, limit=100)     # Make API call.
 
 # Open database connection
-db = pymysql.connect("localhost", "user", "password", "db", autocommit=True)
+db = pymysql.connect("localhost", "root", "limegreen715", "sys", autocommit=True)
 # prepare a cursor object using cursor() method
 cursor = db.cursor()
 
@@ -43,6 +43,15 @@ for result in results['matches']:
         server = result['http']['server']
     except:
         server = None
+    
+    '''print("ASSET_INFO: ")
+    print(ip_str)
+    print(port)
+    print(transport)
+    print(product)
+    print(version)
+    print(server)
+    print("\n")'''
 
     
 
@@ -52,20 +61,36 @@ for result in results['matches']:
     entry_id=cursor.lastrowid
 
 
+    count = 0
     #FILL cve_info TABLE and cve_references
     try:
-        
         for cve in result['vulns']:
-
+            count = count + 1
             sum = result['vulns'][cve]['summary']
 
             cursor.execute("Insert into cve_info(entry_id, cve, summary) values(%d, '%s', '%s')"% (entry_id, cve, sum))
             db.commit()	
             cve_id=cursor.lastrowid
+            # print(cve_id)
+            count2 = 0
+            for reference in result['vulns'][cve]['references']:
+                count2 = count2 + 1
+                cursor.execute("Insert into cve_references(cve_id, reference) values(%d, '%s')"% (cve_id, reference))
+                db.commit()	
 
+            if(count2 > 0):
+                print("CVE ID: ", cve_id)
+                print("Reference count: ", count2)
+                print("\n")
+
+        print("Entry ID: ", entry_id)
+        print("CVE count: ", count)
+        print("\n")
                 
     except:
         continue
+
+    
 
 # disconnect from server
 db.close()
--- paid_api_test.py
+++ paid_api_test.py
@@ -1,11 +1,11 @@
 import shodan
 
-SHODAN_API_KEY = "insert API key"     # Shodan.io API key for auth.
+SHODAN_API_KEY = "UUFuyEaMCkYneMqmlNTImLNl5CwLdpGl"     # Shodan.io API key for auth.
 query = "net:129.59.0.0/16"    # The service to query for.
 api = shodan.Shodan(SHODAN_API_KEY)     # Create API object.
-results = api.search(query, limit=5)     # Make API call.
+results = api.search(query, limit=15)     # Make API call.
+# results = api.host(query, limit=5)
 
-keys = ["ip_str", "org", "isp", "port", "transport", "os", "product", "version", "domains", "hostnames"]
 
 for result in results['matches']:
     for key in keys:
@@ -14,24 +14,28 @@ for result in results['matches']:
         except:
             print(key + ": " + "NULL")
     try:
-        print("city: " + str(result['location']['city']))
-        print("country: " + str(result['location']['country_name']))
-    except:
-        print("location: NULL")
-    try:
         print("server: " + str(result['http']['server']))
     except:
         print("server: NULL")
     try:
         vuln_count = 0
+        '''if(len(results['vulns'])):
+            print("in here")'''
         for cve in result['vulns']:
             if (vuln_count == 3):
                 break
+            # result['vulns'][cve]['references'].length()
+            # result['vulns'][cve]['references'][0]
             print(cve, end='\t')
+            print(result['vulns'][cve]['references'][0])
+            print(result['vulns'][cve]['references'][1])
+            print(result['vulns'][cve]['references'][2])
             vuln_count = vuln_count + 1
     except:
         print("vulns: NULL")
     print('\n')
+
+   
 
 '''
 file = open("data.txt", "w")
